8392c2a47848b115d9737d5510a7a0b70a8c0ff8
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-07-03 07:06:20 -0700

    impl CounterProvider class

--- lib/providers/counter_provider.dart
+++ lib/providers/counter_provider.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/cupertino.dart';
 
 class CounterProvider with ChangeNotifier{
+  int _number = 0;
+  String get number => _number.toString();
 
+  increaseNumber(){
+   _number++;
+   notifyListeners();
+  }
+
+  decreaseNumber(){
+   _number--;
+   notifyListeners();
+  }
+
+  resetNumber(){
+   _number = 0;
+   notifyListeners();
+  }
 }
