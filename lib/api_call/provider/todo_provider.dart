4294d722d600ca978021e5d9eec9056072fd3bc2
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-07-10 00:14:35 -0700

    completed TodoProvider for api call

---
+++ lib/api_call/provider/todo_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_provider_example/api_call/service/todo_service.dart';
+import '../model/todo_model.dart';
+
+class TodoProvider extends ChangeNotifier{
+  final TodoService _service = TodoService();
+  bool isLoading = false;
+  List<TodoModel> _allTodo = [];
+  List<TodoModel> get todoList => _allTodo;
+
+  Future<void> getAllTodo() async {
+    isLoading = true;
+    notifyListeners();
+    final response =  await _service.getAll();
+
+    _allTodo = response;
+    isLoading = false;
+    notifyListeners();
+  }
+
+}
